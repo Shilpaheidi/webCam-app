d16b75e3b91ded48cdd8734e97710b6dfdf40d0e
Shilpaheidi <shilpan048@gmail.com>
2023-10-10 21:23:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'web-cam';
+  private trigger: Subject<any> = new Subject();
+  public webCamImage!: WebcamImage;
+  private nextWebCam: Subject<any> = new Subject();
+  sysImage = '';
+  ngOnInit() {}
+
+  public getSnapshot(): void {
+    this.trigger.next(void 0);
+  }
+
+  public captureImg(webCamImage: WebcamImage): void {
+    this.webCamImage = webCamImage;
+    this.sysImage = webCamImage.imageAsDataUrl;
+    console.info('got webcam  image',this.sysImage);
+    
+  }
+
+  public get invokeobserverable(): Observable<any>{
+   return this.trigger.asObservable();
+  }
+
+  public get nextwebcamobservable():Observable<any>{
+    return this.nextWebCam.asObservable();
+  }
 }
